e54dfc856822535d4e989084e3f9273d898922be
Ryei Estevan <127212997+RyeiEstevan@users.noreply.github.com>
2024-08-28 13:51:49 -0300

    Update Point.h

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -1,39 +1,39 @@
-/**
- * Author: Ulf Lundstrom
- * Date: 2009-02-26
- * License: CC0
- * Source: My head with inspiration from tinyKACTL
- * Description: Class to handle points in the plane.
- * 	T can be e.g. double or long long. (Avoid int.)
- * Status: Works fine, used a lot
- */
-#pragma once
+const double inf = 1e100, eps = 1e-9;
+const long double PI = acos(-1.0L);
+int cmp(double a, double b = 0){
+    if(abs(a-b) < eps)return 0;
+    return (a<b?-1:1);
+}
 
-template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
-template<class T>
-struct Point {
-	typedef Point P;
-	T x, y;
-	explicit Point(T x=0, T y=0) : x(x), y(y) {}
-	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
-	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
-	P operator+(P p) const { return P(x+p.x, y+p.y); }
-	P operator-(P p) const { return P(x-p.x, y-p.y); }
-	P operator*(T d) const { return P(x*d, y*d); }
-	P operator/(T d) const { return P(x/d, y/d); }
-	T dot(P p) const { return x*p.x + y*p.y; }
-	T cross(P p) const { return x*p.y - y*p.x; }
-	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
-	T dist2() const { return x*x + y*y; }
-	double dist() const { return sqrt((double)dist2()); }
-	// angle to x-axis in interval [-pi, pi]
-	double angle() const { return atan2(y, x); }
-	P unit() const { return *this/dist(); } // makes dist()=1
-	P perp() const { return P(-y, x); } // rotates +90 degrees
-	P normal() const { return perp().unit(); }
-	// returns point rotated 'a' radians ccw around the origin
-	P rotate(double a) const {
-		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
-	friend ostream& operator<<(ostream& os, P p) {
-		return os << "(" << p.x << "," << p.y << ")"; }
+struct Point{
+    int x, y;
+    Point(double x=0, double y=0) : x(x), y(y){}
+    Point(const Point& p):x(p.x), y(p.y){}
+    bool operator < (const Point& p) const{
+        if(cmp(x, p.x) != 0)return x<p.x;
+        return cmp(y, p.y) < 0;
+    }
+    bool operator == (const Point& p) const{
+        if(cmp(x, p.x) == 0 && cmp(y, p.y) == 0)return true;
+        return false;
+    }
+    bool operator != (const Point& p)const {return !(p == *this);}
+    Point operator + (const Point& p) const {return Point(x+p.x,y+p.y);}
+	Point operator - (const Point& p) const {return Point(x-p.x,y-p.y);}
+	Point operator * (const double k) const {return Point(x*k,y*k);}
+	Point operator / (const double k) const {return Point(x/k,y/k);}
 };
+
+double dot (const Point& p,const Point& q) { return p.x*q.x + p.y*q.y; }
+double cross (const Point& p,const Point& q) { return p.x*q.y - p.y*q.x; }
+double norm(const Point& p) { return hypot(p.x,p.y); }
+double dist(const Point& p, const Point& q) { return hypot(p.x-q.x,p.y-q.y); }
+double dist2(const Point& p, const Point& q) { return dot(p-q,p-q); }
+Point perp(const Point& p) { return Point(-p.x,p.y); }
+Point normalize(const Point &p) { return p/hypot(p.x, p.y); }
+double angle (const Point& p, Point& q) { return atan2(cross(p, q), dot(p, q)); }
+long double angle (const Point& p) {
+    if(p.x < 0 && p.y == 0)return PI;
+    else if(p.x == 0 && p.y == 0)return 0;
+    return atan2l(p.y, p.x); 
+}
