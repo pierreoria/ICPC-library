79c67e95d8ca2f0968f49e2a79828e5196c60972
pedrocalheiross <102696539+pedrocalheiross@users.noreply.github.com>
2024-08-30 16:48:36 -0300

    Update Matrix.h
    MatrixFexp.cpp

--- content/data-structures/Matrix.h
+++ content/data-structures/Matrix.h
@@ -1,40 +1,41 @@
-/**
- * Author: Ulf Lundstrom
- * Date: 2009-08-03
- * License: CC0
- * Source: My head
- * Description: Basic operations on square matrices.
- * Usage: Matrix<int, 3> A;
- *  A.d = {{{{1,2,3}}, {{4,5,6}}, {{7,8,9}}}};
- *  vector<int> vec = {1,2,3};
- *  vec = (A^N) * vec;
- * Status: tested
- */
-#pragma once
-
-template<class T, int N> struct Matrix {
-	typedef Matrix M;
-	array<array<T, N>, N> d{};
-	M operator*(const M& m) const {
-		M a;
-		rep(i,0,N) rep(j,0,N)
-			rep(k,0,N) a.d[i][j] += d[i][k]*m.d[k][j];
-		return a;
-	}
-	vector<T> operator*(const vector<T>& vec) const {
-		vector<T> ret(N);
-		rep(i,0,N) rep(j,0,N) ret[i] += d[i][j] * vec[j];
-		return ret;
-	}
-	M operator^(ll p) const {
-		assert(p >= 0);
-		M a, b(*this);
-		rep(i,0,N) a.d[i][i] = 1;
-		while (p) {
-			if (p&1) a = a*b;
-			b = b*b;
-			p >>= 1;
-		}
-		return a;
-	}
+const int MOD=1e9+7;
+const int ms =2;
+ 
+class Matrix{
+ public:
+   ll mat[ms][ms] = {{0,0},{0,0}};
+   //vector<vector<ll>> mat= vector<vector<ll>>(n,vector<ll>(n));
+ 
+   Matrix operator * (const Matrix &p){
+ 
+     Matrix ans;
+     for(int i = 0; i<ms; i++){ 
+       for(int j = 0; j<ms; j++){
+         for(int k = ans.mat[i][j] = 0; k<ms;k++){
+         
+           ans.mat[i][j] = ((ans.mat[i][j] + 1LL * (mat[i][k]%MOD) *(p.mat[k][j]%MOD)))%MOD;
+ 
+         }
+       }
+     }
+     return ans;
+ 
+   }
 };
+ 
+Matrix mfxp(Matrix a, ll b){
+ 
+ Matrix ans;
+ for(int i=0;i<ms;i++){
+   ans.mat[i][i] = 1;
+ }
+ 
+ while(b){
+   if(b&1) ans = ans*a;
+   a = a*a;
+   b>>=1; 
+ }
+ 
+ return ans;
+ 
+}
