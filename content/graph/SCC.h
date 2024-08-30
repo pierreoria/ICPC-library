dee19f6efba3e89e4000bea56e35a6b85dc8507e
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-08-30 17:14:33 -0300

    Update SCC.h

--- content/graph/SCC.h
+++ content/graph/SCC.h
@@ -1,41 +1,120 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-28
+ * Author: Pierre Oriá
+ * Date: 2024-08-30
  * License: CC0
- * Source: Czech graph algorithms book, by Demel. (Tarjan's algorithm)
+ * Source: Paguso
  * Description: Finds strongly connected components in a
  * directed graph. If vertices $u, v$ belong to the same component,
  * we can reach $u$ from $v$ and vice versa.
- * Usage: scc(graph, [\&](vi\& v) { ... }) visits all components
- * in reverse topological order. comp[i] holds the component
- * index of a node (a component only has edges to components with
- * lower index). ncomps will contain the number of components.
  * Time: O(E + V)
- * Status: Bruteforce-tested for N <= 5
+ * Status: tested on Iudex problem, recent codeforces Gym contest (não, sei usar vírgula)
  */
 #pragma once
 
-vi val, comp, z, cont;
-int Time, ncomps;
-template<class G, class F> int dfs(int j, G& g, F& f) {
-	int low = val[j] = ++Time, x; z.push_back(j);
-	for (auto e : g[j]) if (comp[e] < 0)
-		low = min(low, val[e] ?: dfs(e,g,f));
+bitset<mx> vis,repvis;
 
-	if (low == val[j]) {
-		do {
-			x = z.back(); z.pop_back();
-			comp[x] = ncomps;
-			cont.push_back(x);
-		} while (x != j);
-		f(cont); cont.clear();
-		ncomps++;
-	}
-	return val[j] = low;
+void dfs(vector<vector<int>>& adj, stack<int>& ord, int u)
+{
+    vis[u] = 1;
+    for (int v : adj[u]) {
+        if (!vis[v]) {
+            dfs(adj,ord,v);
+        }
+    }
+    ord.push(u);
 }
-template<class G, class F> void scc(G& g, F f) {
-	int n = sz(g);
-	val.assign(n, 0); comp.assign(n, -1);
-	Time = ncomps = 0;
-	rep(i,0,n) if (comp[i] < 0) dfs(i, g, f);
+
+// s (source) eh representante do scc
+void dfs2(vector<vector<int>>& scc, vector<vector<int>>& adjT, vector<int>& rep, int s, int u) 
+{
+    vis[u] = 1;
+    rep[u] = s;
+    scc[s].pb(u);
+
+    for (int v : adjT[u]) {
+        if (!vis[v]) {
+            dfs2(scc,adjT,rep,s,v);
+        }
+    }
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t,n,u,g,v,r,sz,repcnt;
+    char p;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+
+        vis.reset();
+        repvis.reset();
+        vector<vector<int>> adj(n);
+        vector<vector<int>> adjT(n);
+        vector<vector<int>> scc(n);
+        vector<int> rep(n);
+        stack<int> ord;
+        repcnt = 0;
+        string gp; // numero:
+
+        ord.push(-1); // evitar verificacao empty
+
+
+        for (int i = 0; i < n; i++)
+        {
+            cin>>u>>gp;
+
+            gp.pop_back(); // dois pontos
+
+            g = stoi(gp);
+
+            for (int j = 0; j < g; j++)
+            {
+                cin>>v;
+                adj[u].pb(v);
+                adjT[v].pb(u);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i]) {
+                dfs(adj,ord,i);
+            }
+        }
+
+        vis.reset();
+
+        while (ord.top() != -1)
+        {
+            u = ord.top(); ord.pop();
+            if (!vis[u]) {
+                repcnt++;
+                dfs2(scc,adjT,rep,u,u);
+            }
+        }
+
+        cout << repcnt << br;
+        for (int i = 0; i < n; i++)
+        {
+            r = rep[i];
+            if (!repvis[r])
+            {
+                repvis[r] = 1;
+                sz = scc[r].size();
+                sort(scc[r].begin(), scc[r].end());
+                for (int j = 0; j < sz; j++){
+                    cout << scc[r][j] << (j == sz-1 ? '\n' : ' ');
+                }
+            }
+        }
+        cout << br;
+    }
+
+    return 0;
 }
