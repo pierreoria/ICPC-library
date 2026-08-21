c80a71a6d6a70702c563cb3152bff68973a4e0df
Guilherme Maranhão <110748981+gui-maranhao@users.noreply.github.com>
2026-08-20 22:18:19 -0300

    Create Mobius.h

---
+++ content/number-theory/Mobius.h
@@ -0,0 +1,24 @@
+const int MAXV = 1e6 + 5;
+ll mob[MAXV]; 
+bool isprime[MAXV];
+
+void sieve(){
+    
+    for(int i = 0; i < MAXV; i++){
+        mob[i] = 1;
+        isprime[i] = 1;
+    }
+
+    isprime[0] = isprime[1] = 0;
+    mob[1] = 1;
+
+    for(int i = 2; i<MAXV; i++){
+        if(isprime[i]){
+            for(int j = i; j<MAXV; j+=i){
+                if(j > i) isprime[j] = 0;
+                mob[j] *= -1;
+                if((j/i) % i == 0) mob[j] = 0;
+            }
+        }
+    }
+}
