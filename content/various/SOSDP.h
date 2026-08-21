49d97f3b911a96792f3040f6fcb750b29f42ccc4
Guilherme Maranhão <110748981+gui-maranhao@users.noreply.github.com>
2026-08-20 22:31:27 -0300

    Create SOSDP.h

---
+++ content/various/SOSDP.h
@@ -0,0 +1,12 @@
+const int N = 20;
+ll dp[1<<N], iVal[1<<N];
+
+void sosDP(){ // O(N * 2^N) 
+    for(int i=0; i<(1<<N); i++) 
+        dp[i] = iVal[i];
+
+	for(int i=0; i<N; i++)
+		for(int mask=0; mask<(1<<N); mask++)
+			if(mask&(1<<i))
+				dp[mask] += dp[mask^(1<<i)];
+}
